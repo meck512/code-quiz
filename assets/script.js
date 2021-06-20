91c08f7ccb99ecc8e51c5de22b9e5b97bdc5192a
meck512 <m_eck12@outlook.com>
2021-06-20 03:09:54 -0400

    simplified entire layout

--- assets/script.js
+++ assets/script.js
@@ -1,70 +1,147 @@
-// Declared variables
-var score = 0;
-var qIndex = 0;
 
-var qContainer = [
+var questions = [
     {
-        title: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
+        question: "Commonly used data types DO NOT include:",
+        multChoice: ["strings", "booleans", "alerts", "numbers"],
         answer: "alerts"
     },
     {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        question: "The condition in an if / else statement is enclosed within ____.",
+        multChoice: ["quotes", "curly brackets", "parentheses", "square brackets"],
         answer: "parentheses"
     },
     {
-        title: "Arrays in Javascript can be used to store ____.",
-        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        question: "Arrays in Javascript can be used to store ____.",
+        multChoice: ["numbers and strings", "other arrays", "booleans", "all of the above"],
         answer: "all of the above"
     },
     {
-        title: "String values must be enclosed within ____ when being assigned to variables.",
-        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        multChoice: ["commas", "curly brackets", "quotes", "parenthesis"],
         answer: "quotes"
     },
     {
-        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
-        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        question: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        multChoice: ["Javascript", "terminal / bash", "for loops", "console log"],
         answer: "console log"
     },
 
 ];
 
-// Variables
-var container = document.querySelector("#container");
-var qContainer = document.querySelector("#questionsDiv");
-// Time/Timer variables
-var time = document.querySelector("#time");
-// Timer
-var timer = document.querySelector("#startTime");
-var timeLeft = 76;
-var timeInterval = 0;
-var timePenalty = 10;
-// Create new UL for qContainer
-var ulCreate = document.createElement("ul");
 
-// Start timer on click (function). Set timer to 75 seconds from 0 default. Display time remaining to user. When time runs out, end interval and notify user.
-timer.addEventListener("click", function () {
-    if (timeInterval === 0) {
-        timeInterval = setInterval(function () {
+
+
+
+
+
+var timerEl = document.getElementById('countdown');
+var startTimer = document.querySelector("#startTimerButton");
+
+function countdown() {
+    var timeLeft = 75;
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+        // As long as the `timeLeft` is greater than 1
+        if (timeLeft > 1) {
+            // Set the `textContent` of `timerEl` to show the remaining seconds
+            timerEl.textContent = timeLeft + ' seconds';
+            // Decrement `timeLeft` by 1
             timeLeft--;
-            time.textContent = "Time: " + timeLeft;
+        } else if (timeLeft === 1) {
+            // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+            timerEl.textContent = timeLeft + ' second';
+            timeLeft--;
+        } else {
+            // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+            timerEl.textContent = '';
+            // Use `clearInterval()` to stop the timer
+            clearInterval(timeInterval);
+            timerEl.textContent = "GAME OVER";
+        }
+    }, 1000);
 
-            if (timeLeft <= 0) {
-                clearInterval(timeInterval);
-                allDone();
-                time.textContent = "GAME OVER";
-            }
-        }, 1000);
+    // Begin quiz when timer starts
+    quiz(qIndex);
+}
+
+
+
+
+
+
+var score = 0;
+var ulCreate = document.createElement("ul");
+var qIndex = 0;
+var qDiv = document.querySelector("#qDiv");
+
+function quiz(qIndex) {
+    // clear HTML elements to make room for question and multChoice
+    qDiv.innerHTML = "";
+    ulCreate.innerHTML = "";
+    // Loop over every question object
+    for (var i = 0; i < questions.length; i++) {
+        // Display current question to user and multChoice
+        var spawnQuestion = questions[qIndex].question;
+        var spawnMultChoice = questions[qIndex].multChoice;
+        qDiv.textContent = spawnQuestion;
     }
-    // return qIndex
-    // **render instead (better for multiple items)
-    render(qIndex);
-});
+
+}
+
+
+
+
+
+// *******************CURRENT BUG - cannot get new li elements to appear on page**********
+function spawnToPage(qIndex) {
+    // Clears existing data in HTML
+    qDiv.innerHTML = "";
+    ulCreate.innerHTML = "";
+    // Loop through array
+    for (var i = 0; i < questions.length; i++) {
+        // Append question
+        var spawnQuestion = questions[qIndex].question;
+        var spawnMultChoice = questions[qIndex].multChoice;
+        qDiv.textContent = spawnQuestion;
+    }
+    // New li's for multChoice 
+    spawnMultChoice.forEach(function (newLi) {
+        var listItem = document.createElement("li");
+        listItem.textContent = newLi;
+        qDiv.appendChild(ulCreate);
+        ulCreate.appendChild(listItem);
+        listItem.addEventListener("click", (check));
+    })
+}
 
 
 
 
 
 
+var timePenalty = 10;
+
+function check(choice) {
+    var userSubmit = choice.target;
+
+    if (userSubmit.matches("li")) {
+        var feedback = document.createElement("div");
+        feedback.setAttribute("id", "feedback");
+       
+        if (userSubmit.textContent == questions[qIndex].answer) {
+            score++;
+            // ^how much is added to the score?
+            feedback.textContent = "Correct!"
+        } else {
+            timeLeft = timeLeft - timePenalty;
+            feedback.textContent = "Nope!"
+        }
+    }
+
+}
+
+
+
+
+
+startTimer.onclick = countdown;
